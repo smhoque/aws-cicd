1c520ad755f24f83d26789c1b0388b6fef303a02
emdadripon <eriponctg@gmail.com>
2025-12-15 16:34:52 -0500

    code change

--- src/main/java/com/javatechie/controller/CourseController.java
+++ src/main/java/com/javatechie/controller/CourseController.java
@@ -65,7 +65,7 @@ public class CourseController {
 
     @GetMapping("/welcome")
     public String greetings() {
-        return "Hello Techie , AWS CICD Example working fine !";
+        return "Hello java Techie , AWS CICD demo Example working fine !";
     }
 
 
